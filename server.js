20102482c30aaec5ca8aab5d0478b592ffdf2efc
Tristan <magombotristan@gmail.com>
2019-06-16 08:55:32 -0700

    Finished scrape using cnn us news

--- server.js
+++ server.js
@@ -3,40 +3,30 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
 var db = require("./models/Article");
 
 var PORT = 3000;
 
-// Initialize Express
 var app = express();
 
-// Configure middleware
 
 
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
-// Routes
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webscrape";
 
-// A GET route for scraping the echoJS website
+mongoose.connect(MONGODB_URI);
+
+
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("https://www.nba.com/news").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
+  axios.get("https://www.cnn.com/us").then(function(response) {
     var $ = cheerio.load(response.data);
-    // Now, we grab every h2 within an article tag, and do the following:
     $("div h3").each(function(i, element) {
-      // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
         .children("a")
         .text();
@@ -44,51 +34,41 @@ app.get("/scrape", function(req, res) {
         .children("a")
         .attr("href");
         console.log(result.title);
-
-      // Create a new Article using the `result` object built from scraping
-      db.create(result)
+        if(result.link.indexOf('index.html')> -1){
+          if(result.link.indexOf('/us/')> -1){
+                db.create(result)
         .then(function(dbArticle) {
-          // View the added result in the console
           console.log(dbArticle);
         })
         .catch(function(err) {
-          // If an error occurred, log it
           console.log(err);
         });
+      };};
     });
-
-    // Send a message to the client
     res.send("Scrape Complete");
   });
 });
 
-// Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
-  // Grab every document in the Articles collection
   db.find({})
     .then(function(dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.findOne({ _id: req.params.id })
     .then(function(dbArticle) {
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
-// Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
